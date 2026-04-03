0c41634d8ec1b3b94a3b734bd5fb4ecf83d99cbf
sanskard7 <sanskardongare7@gmail.com>
2026-04-03 22:13:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-// Simple scroll animation
-document.querySelectorAll('a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
-});
+// Typing effect
+const words = ["Sanskar", "Developer", "Engineer"];
+let i = 0, j = 0, current = "", isDeleting = false;
+const typing = document.getElementById("typing");
+
+function type() {
+  if (i < words.length) {
+    if (!isDeleting && j <= words[i].length) {
+      current = words[i].substring(0, j++);
+    } else if (isDeleting && j >= 0) {
+      current = words[i].substring(0, j--);
+    }
+    typing.innerHTML = current;
+
+    if (j == words[i].length) isDeleting = true;
+    if (j == 0) { isDeleting = false; i++; }
+  } else i = 0;
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+type();
